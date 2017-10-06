190d59cd51250c7c8954892ca5b18c140f0fb722
dgotoole88 <cell321@hotmail.com>
2017-10-07 08:52:04 +1000

    Delete usernameTaken.php

--- twoWheelNav/php/Controller/usernameTaken.php
+++
@@ -1,8 +0,0 @@
-<?php
-if(isset($_GET) & !empty($_POST)){
-    if($_SESSION['count'] == 0){
-        //include 'errorUsernameTaken.php';
-        echo 'Damn, username is already taken';
-    }
-}
-?>
