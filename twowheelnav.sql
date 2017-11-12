36a6857fdd6f37daec584c4a29188e9cbbffb1bc
dgotoole88 <cell321@hotmail.com>
2017-11-13 07:04:58 +1000

    Add files via upload

--- twowheelnav.sql
+++ twowheelnav.sql
@@ -10,25 +10,10 @@ Target Server Type    : MYSQL
 Target Server Version : 50505
 File Encoding         : 65001
 
-Date: 2017-11-11 23:02:31
+Date: 2017-11-13 07:01:51
 */
 
 SET FOREIGN_KEY_CHECKS=0;
--- ----------------------------
--- Table structure for `category`
--- ----------------------------
-DROP TABLE IF EXISTS `category`;
-CREATE TABLE `category` (
-  `categoryID` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `categoryName` varchar(255) NOT NULL,
-  `categoryDescription` varchar(255) NOT NULL,
-  PRIMARY KEY (`categoryID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- ----------------------------
--- Records of category
--- ----------------------------
-
 -- ----------------------------
 -- Table structure for `login`
 -- ----------------------------
@@ -38,32 +23,15 @@ CREATE TABLE `login` (
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   PRIMARY KEY (`loginID`)
-) ENGINE=InnoDB AUTO_INCREMENT=258 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=261 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Records of login
 -- ----------------------------
 INSERT INTO `login` VALUES ('252', 'admin', '$2y$10$w2SRtiyb7l6y7gkfn/5A..cRb5K5g0ZWiS92wMqGZDm4pzEhQyeZC');
 INSERT INTO `login` VALUES ('257', 'bob', '$2y$10$fQRH3lvOMLN4X1i8Vv4lpe3tD90kKIWKcRc8qntcDAP6.1l0DGI/i');
-
--- ----------------------------
--- Table structure for `pointofinterest`
--- ----------------------------
-DROP TABLE IF EXISTS `pointofinterest`;
-CREATE TABLE `pointofinterest` (
-  `pointOfInterestID` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `pointOfInterestName` varchar(255) NOT NULL,
-  `latitude` decimal(8,6) NOT NULL,
-  `longitude` decimal(9,6) NOT NULL,
-  `categoryID` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`pointOfInterestID`),
-  KEY `categoryID` (`categoryID`),
-  CONSTRAINT `categoryID` FOREIGN KEY (`categoryID`) REFERENCES `category` (`categoryID`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- ----------------------------
--- Records of pointofinterest
--- ----------------------------
+INSERT INTO `login` VALUES ('258', 'dgotoole', '$2y$10$/qZDQ/cmWB9z7LI2mPgLQuE6k0LUwZVzAlI6IBpM0cFxqPmUSz07G');
+INSERT INTO `login` VALUES ('260', 'kesaunders', '$2y$10$UgYSFhfoc7tizrmZVuDTVejHSoDLKxqakay4.Q03FJByMuF.Xky.m');
 
 -- ----------------------------
 -- Table structure for `savedroute`
@@ -78,13 +46,14 @@ CREATE TABLE `savedroute` (
   PRIMARY KEY (`routeID`),
   KEY `touristID` (`touristID`),
   CONSTRAINT `touristID` FOREIGN KEY (`touristID`) REFERENCES `tourist` (`touristID`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=79 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=82 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Records of savedroute
 -- ----------------------------
-INSERT INTO `savedroute` VALUES ('75', 'Launceston, Tasmania, Australia', 'Hobart, Tasmania, Australia', 'LongRoad', '199');
 INSERT INTO `savedroute` VALUES ('77', 'Kingston, Tasmania, Australia', 'Kettering, Tasmania, Australia', 'Wooos', '199');
+INSERT INTO `savedroute` VALUES ('80', 'Bicheno, Tasmania, Australia', 'Oatlands, Tasmania, Australia', 'Fun', '199');
+INSERT INTO `savedroute` VALUES ('81', 'Hobart, Tasmania, Australia', 'Launceston, Tasmania, Australia', 'qwerty', '205');
 
 -- ----------------------------
 -- Table structure for `tourist`
@@ -100,10 +69,12 @@ CREATE TABLE `tourist` (
   PRIMARY KEY (`touristID`),
   KEY `loginID` (`loginID`),
   CONSTRAINT `loginID` FOREIGN KEY (`loginID`) REFERENCES `login` (`loginID`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=205 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=208 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Records of tourist
 -- ----------------------------
 INSERT INTO `tourist` VALUES ('199', 'David', 'OToole', 'cell321@hotmail.com', '../View/images/profilePictures/adminORXZ310.jpg', '252');
 INSERT INTO `tourist` VALUES ('204', 'David', 'OToole', 'cell321@hotmail.com', '../View/images/profilePictures/unknown.jpg', '257');
+INSERT INTO `tourist` VALUES ('205', 'David', 'OToole', 'cell321@hotmail.com', '../View/images/profilePictures/unknown.jpg', '258');
+INSERT INTO `tourist` VALUES ('207', 'David', 'OToole', 'cell321@hotmail.com', '../View/images/profilePictures/unknown.jpg', '260');
